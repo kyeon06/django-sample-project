b8279d3758b24bb7c08d4c96350f2f1ccb8f2652
kyeon06 <jkyeon06@gmail.com>
2023-12-12 14:43:18 +0900

    :white_check_mark: test : 회원가입, 로그인 성공 TestCase 작성

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from users.views import LoginView, SignupAPIView
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='user-signup'),
-    path('login/', LoginView.as_view(), name='user_login'),
-    # path('logout/', LogoutView.as_view(), name='user_logout')
+    path('login/', LoginView.as_view(), name='user-login'),
+    # path('logout/', LogoutView.as_view(), name='user-logout')
 ]
